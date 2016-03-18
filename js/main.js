d060ea0486c8967e2db8dcf84e307039065806aa
Kyle-Jackson <kylejackson@Kyles-MacBook-Pro.local>
2016-03-18 09:32:01 -0400

    Fixed all errors.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 var $body = $('body');
-$('html') .on('keydown', function (e) {
+
+$('html').on('keydown', function (e) {
   var $newBall = $('<div>');
   var newLeft = Math.floor(Math.random() * (1000 - 0)) + 0;
   var newTop = Math.floor(Math.random() * (1000 - 0)) + 0;
 
   $newBall.addClass('ball');
-  $body.append ($newBall)
+  $body.append($newBall);
   $newBall.css('left', newLeft);
   $newBall.css('top', newTop);
 
