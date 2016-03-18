7c10167acdfe23146f50f72ed45bfa5bee08bb74
Kyle-Jackson <kylejackson@Kyles-MacBook-Pro.local>
2016-03-18 09:30:37 -0400

    Completed, circle appear randomly.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+var $body = $('body');
 $('html') .on('keydown', function (e) {
   var $newBall = $('<div>');
-  var $newLeft = Math.floor(Math.random() * (1000 - 0)) + 0;
-  var $newTop = Math.floor(Math.random() * (1000 - 0)) + 0;
+  var newLeft = Math.floor(Math.random() * (1000 - 0)) + 0;
+  var newTop = Math.floor(Math.random() * (1000 - 0)) + 0;
 
-  $newBall.append($newBall);
+  $newBall.addClass('ball');
+  $body.append ($newBall)
+  $newBall.css('left', newLeft);
+  $newBall.css('top', newTop);
 
 });
